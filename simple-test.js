ee39b5a1f02c5bf4635b4130589a71af946e6853
Roman F <roman.feliz@hologic.com>
2024-12-03 11:12:13 -0500

    housekeeping

--- simple-test.js
+++ simple-test.js
@@ -2,15 +2,15 @@ import http from "k6/http";
 import { check } from "k6";
 
 export let options = {
-  vus: 100, // Virtual users
-  duration: "30s", // Test duration
+  vus: 100,
+  duration: "30s",
   cloud: {
-    projectID: 3727725, // Your project ID in K6 cloud
-    name: "My First Test", // Test suite name
+    projectID: 3727725,
+    name: "My First Test",
   },
 };
 
 export default function () {
-  const res = http.get("http://127.0.0.1:8910/api/v1/");
+  const res = http.get("http://localhost:8151/api/v1");
   check(res, { "status is 200": (r) => r.status === 200 });
 }
